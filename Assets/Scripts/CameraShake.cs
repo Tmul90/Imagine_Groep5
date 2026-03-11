dc8012358f0ec416b652c1b335abe9615b0bc5ec
HPotch <maastanr@gmail.com>
2026-03-11 18:30:39 +0100

    Added Overstim Logic

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private float posStrength;
+    private float rotStrength;
+    private float time;
+    private float endSmooth;
+
+    private bool shakeWasSet;
+    private Vector2 shakePos;
+    private float shakeRot;
+    
+    private void Update()
+    {
+        time -= Time.deltaTime;
+        time = Mathf.Max(time, 0f);
+
+        if (time > 0f || shakeWasSet)
+        {
+            // Reset, otherwise it'll be additive.
+            transform.localPosition -= new Vector3(shakePos.x, shakePos.y, 0f);
+            transform.localEulerAngles -= new Vector3(0f, 0f, shakeRot);
+            
+            // Set shake
+            shakePos = new Vector2(Random.Range(-posStrength, posStrength), Random.Range(-posStrength, posStrength));
+            shakeRot = Random.Range(-rotStrength, rotStrength);
+            transform.localPosition += new Vector3(shakePos.x, shakePos.y, 0f);
+            transform.localEulerAngles += new Vector3(0f, 0f, shakeRot);
+        }
+    }
+    
+    
+    public void StartShake(float positionStrength, float rotationStrength, float duration, float endSmoothness)
+    {
+        posStrength = positionStrength;
+        rotStrength = rotationStrength;
+        time = duration;
+        endSmooth = endSmoothness;
+    }
+
+    public void SetShake(float positionStrength, float rotationStrength)
+    {
+        posStrength = positionStrength;
+        rotStrength = rotationStrength;
+        shakeWasSet = true;
+    }
+}
--- Assets/Scripts/OverStim.cs
+++ Assets/Scripts/OverStim.cs
@@ -1,6 +1,87 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal; 
 
 public class OverStim : MonoBehaviour
 {
+    // References
+    [SerializeField] private Volume globalVolume;
+    public Transform player;
+    public GreenZones greenZones;
+    private Camera cam;
+    private CameraShake shake;
+    // Variables
+    public float stimulationPercentage = 0f; // Should be between 0 and 100
+    [SerializeField] private AnimationCurve stimCurve; // Adding stimulation is based on height, calculated with this curve, 0 is minHeight, 1 is maxHeight. Value is added every second.
+    [SerializeField] private Vector2 stimHeightBounds; // x : minHeight, y : maxHeight
+    [SerializeField] private float addStimulationSpeed = 1.3f;
+    [SerializeField] private float stimulationRecoverySpeed = 10f;
+    [SerializeField] private AnimationCurve focusDistanceCurve;
+    // Adjustments
+    private ChromaticAberration CA;
+    private LensDistortion LD;
+    private Vignette VN;
+    private DepthOfField DOF;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        shake = cam.GetComponent<CameraShake>();
+    }
+    
+    private void Update()
+    {
+        AddStimulation();
+        VisualFeedback();
+        print(stimulationPercentage);
+    }
+
+    private void AddStimulation()
+    {
+        if (greenZones.inGreenZone)
+        {
+            stimulationPercentage -= stimulationRecoverySpeed * Time.deltaTime;
+        } else
+        {
+            float playerHeight = player.position.y;
+            float clampedHeight = Mathf.Clamp(playerHeight, stimHeightBounds.x, stimHeightBounds.y);
+            float curvePosition = (clampedHeight - stimHeightBounds.x) / (stimHeightBounds.y - stimHeightBounds.x);
+            float addStimulation = stimCurve.Evaluate(curvePosition);
+        
+            stimulationPercentage += addStimulation * Time.deltaTime * addStimulationSpeed;
+        }
+        
+        stimulationPercentage = Mathf.Clamp(stimulationPercentage, 0f, 100f);
+    }
+
+    private void VisualFeedback()
+    {
+        float s = stimulationPercentage;
+        
+        globalVolume.profile.TryGet(out CA);
+        globalVolume.profile.TryGet(out LD);
+        globalVolume.profile.TryGet(out VN);
+        globalVolume.profile.TryGet(out DOF);
+        if(CA != null && LD != null && VN != null && DOF != null)
+        {
+
+            CA.intensity.value = s / 100;
+            LD.intensity.value = (s / 100) * 0.5f;
+            VN.intensity.value = s / 100 * 0.45f;
+            DOF.focusDistance.value = focusDistanceCurve.Evaluate(s / 100) * 100;
+        }
+
+        if (s > 50)
+        {
+            float posStrength = (s - 50) * 0.006f;
+            float rotStrength = (s - 50) * 0.004f;
+
+            shake.SetShake(posStrength, rotStrength);
+            
+            cam.fieldOfView = 70 - ((s - 50) * 0.6f);
+            
+        }
+
+    }
     
 }
