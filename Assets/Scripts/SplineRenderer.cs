7b737bdf6e432707aa53cbca7023dbb8e03067dc
HPotch <maastanr@gmail.com>
2026-03-25 00:21:51 +0100

    Area 2 start

--- Assets/Scripts/SplineRenderer.cs
+++ Assets/Scripts/SplineRenderer.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 public class SplineRenderer : MonoBehaviour
 {
     
-    private void Start()
-    {
-        var linePoints : Vector3[] = SomeKindOfSplineFunctionThatReturnsAnArray();
-        var line = new VectorLine ("Spline", linePoints, lineMaterial, lineWidth);
-        Vector.DrawLine (line);
-    }
+
     
 }
