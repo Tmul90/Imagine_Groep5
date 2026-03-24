532cf0a957becf484513d55761e6bf9d96142b41
Techattack46 <bigvyg@gmail.com>
2026-03-25 00:22:11 +0100

    first street changes

--- Assets/Scripts/SplineRenderer.cs
+++ Assets/Scripts/SplineRenderer.cs
@@ -3,11 +3,4 @@ using UnityEngine;
 public class SplineRenderer : MonoBehaviour
 {
     
-    private void Start()
-    {
-        var linePoints : Vector3[] = SomeKindOfSplineFunctionThatReturnsAnArray();
-        var line = new VectorLine ("Spline", linePoints, lineMaterial, lineWidth);
-        Vector.DrawLine (line);
-    }
-    
 }
